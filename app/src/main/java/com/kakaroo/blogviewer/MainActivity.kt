acca3c31a334c7fbc270604294a63a190ee75a67
twins1224 <twins1224@gmail.com>
2022-04-04 23:16:36 +0900

    fix crash while executing coroutine apply coroutine thread pool

--- app/src/main/java/com/kakaroo/blogviewer/MainActivity.kt
+++ app/src/main/java/com/kakaroo/blogviewer/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -22,7 +23,6 @@ import com.kakaroo.blogviewer.databinding.ActivityMainBinding
 import com.kakaroo.blogviewer.entity.Article
 import com.kakaroo.blogviewer.entity.ArticlesTag
 import com.kakaroo.blogviewer.entity.Category
-import com.kakaroo.blogviewer.html.JSoupParser
 import com.kakaroo.blogviewer.utility.Common
 import com.kakaroo.blogviewer.utility.MyUtility
 import kotlinx.coroutines.*
@@ -143,23 +143,29 @@ class MainActivity : AppCompatActivity() {
                         hideKeyboard()  //키보드를 내린다.
                         //binding.etKeyword.setText("")   //Editor를 지운다.
 
-                        val articleTag = mPref.getString("article_tag_key", Common.SELECTOR_ARTICLE_SYNTAX) ?: Common.SELECTOR_ARTICLE_SYNTAX
-                        val categoryTag = mPref.getString("category_tag_key", Common.SELECTOR_CATEGORY_SYNTAX) ?: Common.SELECTOR_CATEGORY_SYNTAX
-                        val articleUrlTag = mPref.getString("articleLink_tag_key", Common.SELECTOR_ARTICLUEURL_SYNTAX) ?: Common.SELECTOR_ARTICLUEURL_SYNTAX
-                        val titleTag = mPref.getString("title_tag_key", Common.SELECTOR_TITLE_SYNTAX) ?: Common.SELECTOR_TITLE_SYNTAX
-                        val dateTag = mPref.getString("date_tag_key", Common.SELECTOR_DATE_SYNTAX) ?: Common.SELECTOR_DATE_SYNTAX
-                        val imageTag = mPref.getString("image_tag_key", Common.SELECTOR_IMAGE_SYNTAX) ?: Common.SELECTOR_IMAGE_SYNTAX
-                        val summaryTag = mPref.getString("summary_tag_key", Common.SELECTOR_SUMMARY_SYNTAX) ?: Common.SELECTOR_SUMMARY_SYNTAX
-                        val pageMaxNum = mPref.getString("page_max_num_key", Common.MAX_PAGE_NUM.toString()) ?: Common.MAX_PAGE_NUM.toString()
+                        val articleTag = mPref.getString(Common.PREF_KEY_ARTICLE_TAG, Common.SELECTOR_ARTICLE_SYNTAX) ?: Common.SELECTOR_ARTICLE_SYNTAX
+                        val categoryTag = mPref.getString(Common.PREF_KEY_CATEGORY_TAG, Common.SELECTOR_CATEGORY_SYNTAX) ?: Common.SELECTOR_CATEGORY_SYNTAX
+                        val articleLinkTag = mPref.getString(Common.PREF_KEY_ARTICLE_LINK_TAG, Common.SELECTOR_ARTICLE_URL_SYNTAX) ?: Common.SELECTOR_ARTICLE_URL_SYNTAX
+                        val titleTag = mPref.getString(Common.PREF_KEY_TITLE_TAG, Common.SELECTOR_TITLE_SYNTAX) ?: Common.SELECTOR_TITLE_SYNTAX
+                        val dateTag = mPref.getString(Common.PREF_KEY_DATE_TAG, Common.SELECTOR_DATE_SYNTAX) ?: Common.SELECTOR_DATE_SYNTAX
+                        val imageTag = mPref.getString(Common.PREF_KEY_IMAGE_TAG, Common.SELECTOR_IMAGE_SYNTAX) ?: Common.SELECTOR_IMAGE_SYNTAX
+                        val summaryTag = mPref.getString(Common.PREF_KEY_SUMMARY_TAG, Common.SELECTOR_SUMMARY_SYNTAX) ?: Common.SELECTOR_SUMMARY_SYNTAX
+                        val pageMaxNum = mPref.getString(Common.PREF_KEY_PAGE_MAX_NUM, Common.MAX_PAGE_NUM.toString()) ?: Common.MAX_PAGE_NUM.toString()
                         val pageMaxNumInt = pageMaxNum.toInt()
 
-                        val articlesTag = ArticlesTag(articleTag, categoryTag, articleUrlTag, titleTag, dateTag, imageTag, summaryTag)
+                        val articlesTag = ArticlesTag(articleTag, categoryTag, articleLinkTag, titleTag, dateTag, imageTag, summaryTag)
 
-                        val url = mPref.getString("blog_url_key", Common.BLOG_MAIN_URL) ?: Common.BLOG_MAIN_URL
+                        val url = mPref.getString(Common.PREF_KEY_INPUT_URL, Common.BLOG_MAIN_URL) ?: Common.BLOG_MAIN_URL
 
                         var bComplete = if(searchType == Common.EditorInputType.NONE) false else true
                         var pageIndex = 0
                         var asyncTryCnt = 0
+
+                        //Thread pool
+                        val dispatcher = newFixedThreadPoolContext(Common.COROUTINE_THREAD_POOL_NUM, "IO")
+
+                        Log.i(Common.MY_TAG, "Coroutine start")
+                        val startTime = System.currentTimeMillis()
 
                         do {
                             var uriTag = ""
@@ -169,46 +175,68 @@ class MainActivity : AppCompatActivity() {
                                 else -> uriTag += Common.PAGE_TAG + ++pageIndex
                             }
 
-//                            if(pageIndex > 1) {
-//                                Thread.sleep(100)
-//                            }
-
                             asyncTryCnt++
                             //Log.d(Common.MY_TAG, "pre: asyncTryCnt[$asyncTryCnt]")
                             binding.btSearch.isEnabled = false
 
-                            CoroutineScope(Dispatchers.IO).launch {
+                            updateTextView(View.VISIBLE)
 
+                            CoroutineScope(Dispatchers.IO).launch() {
                                 try {
                                     withTimeout(Common.HTTP_CRAWLING_TIMEOUT_MILLIS) {
-                                        //Log.e(Common.MY_TAG, "Force to stop dueto Coroutine's Timeout")
-                                        val coroutine = async {
+                                        //Log.i(Common.MY_TAG, "Force to stop due to Coroutine's Timeout")
+
+                                        /*  single thread ; suspend fun 호출
+                                       val coroutine = async {
                                             executeCrawling(url, uriTag, articlesTag) }
-
                                         val result = coroutine.await()
-                                        //Log.e(Common.MY_TAG, "asyncTryCnt[$asyncTryCnt], pageIndex[$pageIndex], result size is ${result.size}")
-
-                                        asyncTryCnt--
+                                        mArticleList.addAll(result)
+                                        Log.i(Common.MY_TAG, "asyncTryCnt[$asyncTryCnt], pageIndex[$pageIndex], result size is ${result.size}")
+                                         */
+                                        
+                                        //dispatcher를 인수로 하는 비동기 함수 호출
+                                        val result = asyncFetchElement(url, uriTag, articlesTag, dispatcher).await()
                                         mArticleList.addAll(result)
 
-                                        mArticleList.sortWith(compareByDescending<Article> {it.date})
+                                        //feeds는 url list로 되었을 때는 아래와 같이 처리
+                                        /*
+                                        var requests = mutableListOf<Deferred<List<Article>>>()
+                                        feeds.mapTo(requests) {
+                                            asyncFetchElement(url, it, articlesTag, dispatcher)
+                                        }
+                                        requests.forEach {
+                                            it.join()   //deferred가 예외를 가질 경우 예외를 갖지 않게 await대신 join으로 처리
+                                        }
+                                        mArticleList = requests.filter{!it.isCancelled}
+                                            .map{it.getCompleted()} as ArrayList<Article>
+                                        */
 
-                                        if(searchType == Common.EditorInputType.NONE)
-                                            mCategoryList.clear()
+                                        asyncTryCnt--
+                                        //Log.e(Common.MY_TAG, "Debug::asyncTryCnt[$asyncTryCnt]")
 
-                                        val mapList = mArticleList.groupBy { it.categoryName }
-                                        //Log.i(Common.MY_TAG, mapList.toString())
+                                        //완료시점에만 collection을 수정하자!!
+                                        //그렇지 않으면 index가 없는 부분에 접근하다가 java.util.ConcurrentModificationException 가 발생한다!!
+                                        if(asyncTryCnt == 0) {
+                                            mArticleList.sortWith(compareByDescending<Article> {it.date})
 
-                                        mapList.forEach{ item -> mCategoryList.add(Category(item.key,
-                                            item.value as ArrayList<Article>
-                                        ))}
+                                            if(searchType == Common.EditorInputType.NONE)
+                                                mCategoryList.clear()
+
+                                            val mapList = mArticleList.groupBy { it.categoryName }
+
+                                            mapList.forEach{ item -> mCategoryList.add(Category(item.key,
+                                                item.value as ArrayList<Article>
+                                            ))}
+                                        }
 
                                         withContext(Dispatchers.Main) {
                                             mAdapter.notifyDataSetChanged()
-
                                             if(asyncTryCnt == 0) {
-                                                Log.d(Common.MY_TAG, "btSearch is enabled")
+                                                mAdapter.notifyDataSetChanged()
+                                                val endTime = System.currentTimeMillis()
+                                                Log.d(Common.MY_TAG, "CoroutineScope is completed : ${endTime-startTime}")
                                                 binding.btSearch.isEnabled = true
+                                                updateTextView(View.INVISIBLE)
                                             }
 
                                             if (result.size == 0 && mCategoryList.isEmpty()) {
@@ -289,8 +317,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    //코루틴 내부에서 실행되는 함수는 suspend로 감싸줘야 함
-    private suspend fun executeCrawling(url: String, uriTag: String, articlesTag: ArticlesTag): ArrayList<Article> {
+    private fun asyncFetchElement(url: String, uriTag: String, articlesTag: ArticlesTag,
+                                  dispatcher: CoroutineDispatcher) = GlobalScope.async(dispatcher) {
         var articleList = ArrayList<Article>()
 
         try {
@@ -302,13 +330,66 @@ class MainActivity : AppCompatActivity() {
 
             val contentElements: Elements = doc.select(articlesTag.articleTag)
             for ((i, elem) in contentElements.withIndex()) {
-                val category = elem.select(articlesTag.categoryTag).text()
-                val articleUrl = elem.select(articlesTag.articleUrlTag).first().attr(Common.HREF_TAG)
-                val title = elem.select(articlesTag.titleTag).text()
-                val date = elem.select(articlesTag.dateTag).text()
-                val categoryUrl = elem.select(articlesTag.categoryTag).first().attr(Common.HREF_TAG)
-                val imageUrl = elem.select(articlesTag.imageTag).attr(Common.SRC_TAG)
-                val summary = elem.select(articlesTag.summaryTag).text()
+                val category = elem.select(articlesTag.categoryTag)?.text() ?: ""
+                val articleUrl = elem.select(articlesTag.articleUrlTag)?.attr(Common.HREF_TAG) ?: ""
+                val title = elem.select(articlesTag.titleTag)?.text() ?: ""
+                val date = elem.select(articlesTag.dateTag)?.text() ?: ""
+                val categoryUrl = elem.select(articlesTag.categoryTag)?.attr(Common.HREF_TAG) ?: ""
+                var imageUrl = elem.select(articlesTag.imageTag)?.attr(Common.SRC_TAG) ?: ""
+                if(imageUrl.isNotEmpty()) {
+                    val subIdx1 = imageUrl.indexOf("https://")
+                    val subIdx2 = imageUrl.indexOf("http://")
+                    imageUrl = if (subIdx1 != -1) imageUrl.substring(subIdx1) else {
+                        if(subIdx2 != -1) imageUrl.substring(subIdx2) else imageUrl
+                    }
+                }
+                val summary = elem.select(articlesTag.summaryTag)?.text() ?: ""
+
+                //mCategorySet.add(category)
+                articleList.add(Article(i, category, title, date, url+categoryUrl, url+articleUrl, imageUrl, summary))
+            }
+
+        } catch (e: IOException) {
+            // HttpUrlConnection will throw an IOException if any 4XX
+            // response is sent. If we request the status again, this
+            // time the internal status will be properly set, and we'll be
+            // able to retrieve it.
+            Log.e(Common.MY_TAG, "Jsoup error: $e")
+        }
+
+        articleList.map{ it }
+
+    }
+
+    //코루틴 내부에서 실행되는 함수는 suspend로 감싸줘야 함
+    private suspend fun executeCrawling(url: String, uriTag: String, articlesTag: ArticlesTag): ArrayList<Article> {
+        var articleList = ArrayList<Article>()
+
+        Log.i(Common.MY_TAG, "executeCrawling:: url:$url uri:$uriTag")
+
+        try {
+            val doc: Document = Jsoup.connect(url+uriTag)
+                .ignoreContentType(true)
+                .get()
+
+            //Log.i(Common.MY_TAG, doc.toString())
+
+            val contentElements: Elements = doc.select(articlesTag.articleTag)
+            for ((i, elem) in contentElements.withIndex()) {
+                val category = elem.select(articlesTag.categoryTag)?.text() ?: ""
+                val articleUrl = elem.select(articlesTag.articleUrlTag)?.attr(Common.HREF_TAG) ?: ""
+                val title = elem.select(articlesTag.titleTag)?.text() ?: ""
+                val date = elem.select(articlesTag.dateTag)?.text() ?: ""
+                val categoryUrl = elem.select(articlesTag.categoryTag)?.attr(Common.HREF_TAG) ?: ""
+                var imageUrl = elem.select(articlesTag.imageTag)?.attr(Common.SRC_TAG) ?: ""
+                if(imageUrl.isNotEmpty()) {
+                    val subIdx1 = imageUrl.indexOf("https://")
+                    val subIdx2 = imageUrl.indexOf("http://")
+                    imageUrl = if (subIdx1 != -1) imageUrl.substring(subIdx1) else {
+                        if(subIdx2 != -1) imageUrl.substring(subIdx2) else imageUrl
+                    }
+                }
+                val summary = elem.select(articlesTag.summaryTag)?.text() ?: ""
 
                 //mCategorySet.add(category)
                 articleList.add(Article(i, category, title, date, url+categoryUrl, url+articleUrl, imageUrl, summary))
@@ -333,11 +414,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun updateTextView(visible: Int) {
+        binding.tvResult.visibility = visible
+    }
+
     /*private fun updateTextView(key: String, size: Int) {
         binding.tvResult.text = "${key}에 관련된 ${size}개의 기사를 찾았습니다."
     }*/
 
-    private fun isNumber(str: String) = !(str.toIntOrNull() == null)
+    private fun isNumber(str: String) = str.toIntOrNull() != null
 
     private fun checkInternetPermissions() {
         val permissionResult = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.INTERNET)
--- app/src/main/java/com/kakaroo/blogviewer/utility/Common.kt
+++ app/src/main/java/com/kakaroo/blogviewer/utility/Common.kt
@@ -10,19 +10,33 @@ object Common {
     const val HREF_TAG                =   "href"
     const val SRC_TAG                 =   "src"
 
+
+    //default value
+    /*
+    const val BLOG_MAIN_URL           =   "https://youngest-programming.tistory.com"
+    const val SELECTOR_ARTICLE_SYNTAX     =   "div.list_content"//<article ....  /article>
+    const val SELECTOR_CATEGORY_SYNTAX    =   "a.link_cate"   //<a href="/category/Kotlin" class="link-category">Kotlin</a>
+    const val SELECTOR_ARTICLE_URL_SYNTAX =   "a.link_post"    //<a href="/69" class="link-article">, attr="href"
+    const val SELECTOR_TITLE_SYNTAX       =   ".tit_post"
+    const val SELECTOR_DATE_SYNTAX       =   ".txt_date"
+    const val SELECTOR_IMAGE_SYNTAX       =   "img"//<img src="https://...png" class="img-thumbnail", attr="src"
+    const val SELECTOR_SUMMARY_SYNTAX     =   "p.txt_post"  //<p class="summary">*/
+
     const val SELECTOR_ARTICLE_SYNTAX     =   "article"//<article ....  /article>
     const val SELECTOR_CATEGORY_SYNTAX    =   "a.link-category"   //<a href="/category/Kotlin" class="link-category">Kotlin</a>
-    const val SELECTOR_ARTICLUEURL_SYNTAX =   "a.link-article"    //<a href="/69" class="link-article">
+    const val SELECTOR_ARTICLE_URL_SYNTAX =   "a.link-article"    //<a href="/69" class="link-article">
     const val SELECTOR_TITLE_SYNTAX       =   ".title"
     const val SELECTOR_DATE_SYNTAX       =   ".date"
     const val SELECTOR_IMAGE_SYNTAX       =   "img.img-thumbnail"//"img[src\$=.png]"  //<img src="https://...png" class="img-thumbnail"
     const val SELECTOR_SUMMARY_SYNTAX     =   ".summary"  //<p class="summary">
 
+    const val COROUTINE_THREAD_POOL_NUM     = 8
+
     const val ARTICLE_URL       = 0
 
     const val REQUEST_INTERNET_PERMISSION   =   10
 
-    const val HTTP_CRAWLING_TIMEOUT_MILLIS  =   5000L
+    const val HTTP_CRAWLING_TIMEOUT_MILLIS  =   10000L
 
     //const val SUBJECT_VIEW = 1
     //const val ARTICLE_VIEW = 2
@@ -34,5 +48,15 @@ object Common {
         PAGE,
         SEARCH
     }
+
+    const val PREF_KEY_INPUT_URL    =   "url_input_key"
+    const val PREF_KEY_ARTICLE_TAG    =   "article_tag_key"
+    const val PREF_KEY_CATEGORY_TAG    =   "category_tag_key"
+    const val PREF_KEY_ARTICLE_LINK_TAG    =   "articleLink_tag_key"
+    const val PREF_KEY_TITLE_TAG    =   "title_tag_key"
+    const val PREF_KEY_DATE_TAG    =   "date_tag_key"
+    const val PREF_KEY_IMAGE_TAG    =   "image_tag_key"
+    const val PREF_KEY_SUMMARY_TAG    =   "summary_tag_key"
+    const val PREF_KEY_PAGE_MAX_NUM    =   "page_max_num_key"
 
 }
